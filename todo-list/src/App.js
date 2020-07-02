dc7ef45826c1231dfc5efe779416161799822062
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-02 16:03:20 -0300

    Update App.js, adding todos components

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import uuid from 'uuid'
 
 function App() {
   return (
-    <div>
-      Hello World from app component
+    <div className="container">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-8 mt-4">
+          <h3 className="text-capitalize text-center">Todo Input</h3>
+          <TodoInput />
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
