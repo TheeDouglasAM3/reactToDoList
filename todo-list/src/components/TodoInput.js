ff544a6678049bd3a23a94a705b20a76d71cf916
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-03 16:40:49 -0300

    Update TodoInput with submit

--- todo-list/src/components/TodoInput.js
+++ todo-list/src/components/TodoInput.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Todoinput extends Component {
+
     render() {
+
+        const {item, handleChange, handleSubmit} = this.props;
+
         return (
             <div className="card card-body my-3">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text bg-primary text-white">
@@ -15,6 +19,8 @@ export default class Todoinput extends Component {
                             type="text" 
                             className="form-control text-capitalize" 
                             placeholder="add a todo item"
+                            value={item}
+                            onChange={handleChange}
                         />
                     </div>
                     <button className="btn btn-block btn-primary mt-3">add item</button>
