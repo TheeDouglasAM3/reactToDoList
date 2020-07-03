cf4caf7e0c3101743e7f0be3e0cdb7dc517293fd
Douglas Marcelino <douglas_champion@hotmail.com>
2020-07-03 16:41:42 -0300

    Update components to show and clean lists
    TodoItem.js
    TodoList.js

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -3,11 +3,31 @@ import TodoItem from './TodoItem'
 
 export default class TodoList extends Component {
     render() {
+
+        const {items, clearList} = this.props
+
         return (
             <ul className="list-group mt-5">
                 <h3 className="text-capitalize text-center">Todo List</h3>
-                <TodoItem />
-                <button className="btn btn-block btn-danger mt-5">clear list</button>
+
+                {
+                    items.map(item => {
+                        return (
+                            <TodoItem 
+                                key={item.id}
+                                title={item.title}
+                            />
+                        )
+                    })
+                }
+
+                
+                <button 
+                    onClick={clearList}
+                    className="btn btn-block btn-danger mt-5"
+                >
+                    clear list
+                </button>
             </ul>
         )
     }
